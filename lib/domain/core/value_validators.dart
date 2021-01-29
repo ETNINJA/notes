4c3fbf7433328ba9426fa68c60f7b2b7534b826b
EtNinja <annealmd@gmail.com>
2021-01-29 15:54:15 -0300

    refactor: made it generic
    add core folders and files
    made auth files use abstract class value_objects in the core folder

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,1 +1,22 @@
+import 'package:dartz/dartz.dart';
+import '../core/failures.dart';
 
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validatePassword(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
