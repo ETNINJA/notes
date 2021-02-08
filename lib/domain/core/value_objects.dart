1d77826b6d4abb196c72f792f25332e01870da15
EtNinja <annealmd@gmail.com>
2021-02-08 16:03:32 -0300

    :hammer: refactor: core/value_objects
    helper func getOrCrash to be used in the infrastructure/auth/ firebase_auth_facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,12 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_DDD/domain/core/errors.dart';
 import 'package:notes_DDD/domain/core/failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  //helper method for firbaseAuthFacade
+  T getOrCrash() {
+    return value.fold(
+        (f) => throw UnexpectedValureError(valueFailure: f), (r) => r);
+  }
 
   @override
   bool operator ==(Object o) {
