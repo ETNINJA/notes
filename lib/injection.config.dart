9b721eff3f1a14fe7fb740cb54185833c288ccd3
EtNinja <annealmd@gmail.com>
2021-02-12 12:07:37 -0300

    :sparkles: feat: injectable module
    infrastructure/core/injectable file to get the concrete implementation os classes from firebase

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -5,11 +5,13 @@
 // **************************************************************************
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'infrastructure/auth/firebase_auth_facade.dart';
+import 'infrastructure/core/firebase_injectable_module.dart';
 import 'domain/auth/i_auth_facade.dart';
 import 'application/auth/sign_in_form/sign_in_form_bloc.dart';
 
@@ -22,8 +24,14 @@ GetIt $initGetIt(
   EnvironmentFilter environmentFilter,
 }) {
   final gh = GetItHelper(get, environment, environmentFilter);
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  gh.lazySingleton<FirebaseAuth>(() => firebaseInjectableModule.firebaseAuth);
+  gh.lazySingleton<FirebaseFirestore>(() => firebaseInjectableModule.firestore);
+  gh.lazySingleton<GoogleSignIn>(() => firebaseInjectableModule.googleSignIn);
   gh.lazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(get<FirebaseAuth>(), get<GoogleSignIn>()));
   gh.factory<SignInFormBloc>(() => SignInFormBloc(get<IAuthFacade>()));
   return get;
 }
+
+class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
